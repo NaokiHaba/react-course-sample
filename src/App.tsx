e45b4ab8b8916e237e94f20dc0841d77298cec17
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-24 01:27:34 +0900

    feat: Add dummyTodoList data file and TodoList component, and define Todo type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { dummyTodoList } from "./data/dummyTodoList";
+import { TodoList } from "./components/TodoList";
+import { Todo } from "./types/todo";
+import { useState } from "react";
+
 function App() {
+	const [todos, setTodos] = useState<Todo[]>(dummyTodoList);
+
+	const changeCompleted = (id: number) => {
+		setTodos((prevTodos) => {
+			return prevTodos.map((todo) => {
+				if (todo.id === id) {
+					return { ...todo, completed: !todo.completed };
+				}
+				return todo;
+			});
+		});
+	};
+
 	return (
-		<div className="p-4">
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
+		<main className="mx-auto mt-10 max-w-xl">
+			<h1 className="text-center text-4xl">Todoアプリ</h1>
+			<div className="rounded bg-slate-200 p-5">
+				<TodoList todos={todos} changeCompleted={changeCompleted} />
+			</div>
+		</main>
 	);
 }
 
