7886e00481ed9b4709565135d377f09a39700ef1
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-24 01:27:25 +0900

    feat: Add dummyTodoList data file

---
+++ src/data/dummyTodoList.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyTodoList: Todo[] = [
+	{
+		id: 1,
+		title: "テスト",
+		completed: false,
+	},
+	{
+		id: 2,
+		title: "テスト2",
+		completed: false,
+	},
+	{
+		id: 3,
+		title: "テスト3",
+		completed: false,
+	},
+];
