ba18ab216fbdd94816ab680172cd83f727757250
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-27 02:43:33 +0900

    refactor: Refactor product type to use separate types for category and product item

--- packages/mercali-clone/src/types/product.ts
+++ packages/mercali-clone/src/types/product.ts
@@ -1,13 +1,17 @@
+export type Category = {
+	id: number;
+	name: string;
+};
+
+export type ProductItem = {
+	code: string;
+	name: string;
+	price: number;
+	imageUrl: string;
+};
+
 export type Product = {
 	name: string;
-	category: {
-		id: number;
-		name: string;
-	};
-	items: {
-		code: string;
-		name: string;
-		price: number;
-		imageUrl: string;
-	}[];
+	category: Category;
+	items: ProductItem[];
 };
