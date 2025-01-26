e097d935b9bc3e3f2aa79e9062e8062527a182e9
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-27 00:32:56 +0900

    refactor: Update App.tsx to include a responsive navigation menu

--- packages/mercali-clone/src/App.tsx
+++ packages/mercali-clone/src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { navigationMenus } from "./constant";
 
 function App() {
-  return (
-    <>
-      Hello World
-    </>
-  )
+	return (
+		<>
+			<div className="min-h-screen" />
+			<nav className="sticky bottom-0 z-10 border-t border-secondary-border bg-white lg:hidden">
+				<ul className="grid grid-cols-4">
+					{navigationMenus.map((menu) => (
+						<li key={menu.label}>
+							<a
+								href={menu.path}
+								className="flex flex-col items-center justify-center gap-1 py-1.5 text-xs hover:bg-secondary-hover"
+							>
+								<menu.icon className="size-[24px] stroke-2" />
+								<span className="text-xs">{menu.label}</span>
+							</a>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</>
+	);
 }
 
-export default App
+export default App;
