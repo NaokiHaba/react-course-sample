975e81efdbf0fffa8b6dfe9fe3bcffb51b4e076d
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-27 02:43:39 +0900

    feat: Add favorites functionality to ProductItem component

--- packages/mercali-clone/src/App.tsx
+++ packages/mercali-clone/src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { Categories } from "./components";
 import { PageHeader, PageNavigation, PageFooter } from "./layouts";
 import { sneakersProductsMaster } from "./data";
 import { Products } from "./components";
+import { favorites } from "./data/favorites";
 
 function App() {
+	const [favoritesProduct, setFavoritesProduct] = useState(favorites);
+
+	const handleAddFavorite = (productCode: string) => {
+		setFavoritesProduct((prev) => [...prev, productCode]);
+	};
+
+	const handleRemoveFavorite = (productCode: string) => {
+		setFavoritesProduct((prev) => prev.filter((code) => code !== productCode));
+	};
+
 	return (
 		<>
 			<div className="sticky top-0 z-10 bg-white">
@@ -14,7 +26,12 @@ function App() {
 				<h2 className="pb-5 text-lg font-bold text-secondary-text lg:text-xl">
 					閲覧した商品からのおすすめ
 				</h2>
-				<Products product={sneakersProductsMaster} />
+				<Products
+					product={sneakersProductsMaster}
+					favoritesProduct={favoritesProduct}
+					handleAddFavorite={handleAddFavorite}
+					handleRemoveFavorite={handleRemoveFavorite}
+				/>
 			</main>
 			<PageNavigation />
 			<PageFooter />
