ac5db161974c0e5aabdc0b7fba428552cbdcc526
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-27 00:33:09 +0900

    refactor: Update navigationMenus.ts to include responsive navigation menu icons

---
+++ packages/mercali-clone/src/constant/navigationMenus.ts
@@ -0,0 +1,24 @@
+import { Home, Bell, Camera, User } from "lucide-react";
+
+export const navigationMenus = [
+	{
+		label: "ホーム",
+		path: "/",
+		icon: Home,
+	},
+	{
+		label: "お知らせ",
+		path: "/",
+		icon: Bell,
+	},
+	{
+		label: "出品",
+		path: "/",
+		icon: Camera,
+	},
+	{
+		label: "マイページ",
+		path: "/",
+		icon: User,
+	},
+];
