e8968421c50afaa24ade321a1252a9c5c06d789b
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-27 02:43:30 +0900

    feat: Add ProductItem component to display individual product information and handle favorites

--- packages/mercali-clone/src/components/Products.tsx
+++ packages/mercali-clone/src/components/Products.tsx
@@ -1,26 +1,35 @@
 import { Product } from "../types";
 import { ProductsHeader } from "./ProductsHeader";
+import { ProductItem } from "./ProductItem";
 
 type Props = {
 	product: Product;
+	favoritesProduct: string[];
+	handleAddFavorite: (productCode: string) => void;
+	handleRemoveFavorite: (productCode: string) => void;
 };
 
-export const Products = ({ product }: Props) => {
+export const Products = ({
+	product,
+	favoritesProduct,
+	handleAddFavorite,
+	handleRemoveFavorite,
+}: Props) => {
 	return (
 		<section className="space-y-6 border-t border-secondary-border py-6">
 			<ProductsHeader product={product} />
-			<ul
-				// ウィンドウの横幅によって列数を変える
-				className="grid grid-cols-[repeat(3,1fr)] gap-1 sm:grid-cols-[repeat(4,1fr)] lg:grid-cols-[repeat(5,1fr)] lg:gap-6"
-			>
+			<ul className="grid grid-cols-[repeat(3,1fr)] gap-1 sm:grid-cols-[repeat(4,1fr)] lg:grid-cols-[repeat(5,1fr)] lg:gap-6">
 				{product.items.map((item) => (
 					<li
 						key={item.code}
-						// ウィンドウの横幅によって列数が変わるため、2行になるように表示する商品数を変える
 						className="hidden lg:[&:nth-child(-n+10)]:block [&:nth-child(-n+6)]:block sm:[&:nth-child(-n+8)]:block"
 					>
-						<img src={item.imageUrl} />
-						{item.name}
+						<ProductItem
+							product={item}
+							favoritesProduct={favoritesProduct}
+							handleAddFavorite={handleAddFavorite}
+							handleRemoveFavorite={handleRemoveFavorite}
+						/>
 					</li>
 				))}
 			</ul>
