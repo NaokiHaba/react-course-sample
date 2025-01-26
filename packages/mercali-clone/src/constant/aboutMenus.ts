6afaf7191776d9adf946cb1b7c76194e5cc7b4e2
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-27 02:16:28 +0900

    feat: Add aboutMenus, helpMenus, and termsMenus constants

---
+++ packages/mercali-clone/src/constant/aboutMenus.ts
@@ -0,0 +1,40 @@
+import { Menu } from "../types";
+
+export const aboutMenus: Menu[] = [
+	{
+		label: "会社概要（運営会社）",
+		path: "/",
+	},
+	{
+		label: "採用情報",
+		path: "/",
+	},
+	{
+		label: "プレスリリース",
+		path: "/",
+	},
+	{
+		label: "公式ブログ",
+		path: "/",
+	},
+	{
+		label: "プレスキット",
+		path: "/",
+	},
+	{
+		label: "カリカリUS",
+		path: "/",
+	},
+	{
+		label: "カリカリShops",
+		path: "/",
+	},
+	{
+		label: "カリカリShops会社概要（運営会社）",
+		path: "/",
+	},
+	{
+		label: "カリカリShopsプレスリリース",
+		path: "/",
+	},
+];
---
+++ packages/mercali-clone/src/constant/helpMenus.ts
@@ -0,0 +1,16 @@
+import { Menu } from "../types";
+
+export const helpMenus: Menu[] = [
+	{
+		label: "ヘルプセンター（ガイド・お問い合わせ）",
+		path: "/",
+	},
+	{
+		label: "カリカリShops出店者向けガイド",
+		path: "/",
+	},
+	{
+		label: "お問い合わせ一覧",
+		path: "/",
+	},
+];
