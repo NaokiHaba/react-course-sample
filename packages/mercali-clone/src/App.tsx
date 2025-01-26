7b3c8d895bbe091a3d8dbd39474e3ffcfd24ecdc
Naoki Haba <naosprintrunner6385@gmail.com>
2025-01-27 02:02:57 +0900

    feat: Add PageHeader component to App.tsx

--- packages/mercali-clone/src/App.tsx
+++ packages/mercali-clone/src/App.tsx
@@ -1,10 +1,11 @@
 import { Categories } from "./components";
-import { PageNavigation } from "./layouts";
+import { PageHeader, PageNavigation } from "./layouts";
 
 function App() {
 	return (
 		<>
 			<div className="sticky top-0 z-10 bg-white">
+				<PageHeader />
 				<Categories />
 			</div>
 			<div className="min-h-screen" />
